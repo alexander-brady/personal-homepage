73fb337f56e1c2ecb55552ed8611d2dcd8442fcb
Alexander Brady <129788423+alexander-brady@users.noreply.github.com>
2025-07-13 00:40:52 +0200

    Update tailwind.config.js
    Added dark mode support

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export const content = ["./templates/**/*.html", "./content/**/*.md"];
+export const darkMode = 'media';
 export const theme = {
   extend: {},
 };
